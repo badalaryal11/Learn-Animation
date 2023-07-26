5fd8f447743c562fd53434cc56fa839edec3ce47
Badal Aryal <badal.aryal@gmail.com>
2023-07-26 14:32:59 +0545

    animation to change the size of the rectangle according to the different duration.

--- Learn Animation/ViewController.swift
+++ Learn Animation/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let myView = UIView (frame: CGRect(x: 0, y: 0, width: 200, height: 200))
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        myView.backgroundColor = .red
+        myView.center = view.center
+        view.addSubview(myView)
+        
+        let myButton = UIButton (frame: CGRect(x: (view.frame.size.width - 200)/2, y: (view.frame.size.height - 220), width: 200, height:200))
+        myButton.backgroundColor = .black
+        myButton.setTitleColor(.white, for: .normal)
+        myButton.setTitle("Start", for: .normal)
+        myButton.addTarget(self, action: #selector(animate), for: .touchUpInside)
+        view.addSubview(myButton)
+    }
+    @objc func animate() {
+        UIView.animate(withDuration: 1,
+                       animations: {
+            self.myView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+        }, completion: { done in
+            if done {
+                UIView.animate(withDuration: 1,
+                               animations: {
+                    self.myView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+                })
+                
+            }
+        })
+    }
 }
-
+            
+   
